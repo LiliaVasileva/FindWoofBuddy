d4f23b603d2ef0b07a5422b317d06b8a1467d5f2
Lilia Vasileva <lilia.vvasileva@gmail.com>
2022-11-03 20:20:20 +0200

    Demo

--- find_buddy/settings.py
+++ find_buddy/settings.py
@@ -8,7 +8,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = os.getenv('SECRET_KEY', '')
 
-DEBUG = os.getenv('DEBUG', 'False') == 'True'
+DEBUG = os.getenv('DEBUG', 'True') == 'True'
 
 
 APP_ENVIRONMENT = os.getenv('APP_ENVIRONMENT', '')
